92d4fef4c3250f5a4deb956d8a9411d9fc5acaaf
Kyle Ames <KyleAmes13@gmail.com>
2017-07-29 18:42:34 -0400

    Add 2017 setup DDL

---
+++ sql/2017.sql
@@ -0,0 +1,18 @@
+INSERT INTO years (year, year_start) VALUES (2017, 1504569600);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 1);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 2);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 3);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 4);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 5);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 6);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 7);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 8);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 9);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 10);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 11);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 12);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 13);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 14);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 15);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 16);
+INSERT INTO weeks (year_id, week) VALUES ((SELECT years.id FROM years WHERE year = 2017), 17);
