e9dc251879a86b09656ad4a69a9edf13212e688d
Kyle Ames <KyleAmes13@gmail.com>
2017-08-12 13:41:20 -0400

    sqlite3: correct column name in SQL query

--- sqlite3/totals.go
+++ sqlite3/totals.go
@@ -28,7 +28,7 @@ func (db Datastore) weekTotals(username string, year int, minWeek int, maxWeek i
 		JOIN users ON picks.user_id = users.id
 		JOIN games ON picks.game_id = games.id
 		JOIN weeks ON games.week_id = weeks.id JOIN years ON weeks.year_id = years.id
-		WHERE users.email LIKE ?1 AND years.year = ?2 AND weeks.week >= ?3 AND weeks.week <= ?4 AND ((games.home_score > games.away_score AND picks.selection_id = games.home_id) OR (games.home_score < games.away_score AND picks.selection_id = games.away_id))
+		WHERE users.email LIKE ?1 AND years.year = ?2 AND weeks.week >= ?3 AND weeks.week <= ?4 AND ((games.home_score > games.away_score AND picks.selection = games.home_id) OR (games.home_score < games.away_score AND picks.selection = games.away_id))
 		GROUP BY users.email, weeks.week`
 
 	rows, err := db.Query(sql, username, year, minWeek, maxWeek)
