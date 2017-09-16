b449c2409d1c0a4343a895f9c1862009c18dab63
Kyle Ames <KyleAmes13@gmail.com>
2017-09-16 12:08:21 -0400

    nfl: implement schedule results download and importing

--- cmd/nfl/schedule.go
+++ cmd/nfl/schedule.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ameske/nfl-pickem/parser/results"
 	"github.com/ameske/nfl-pickem/parser/schedule"
 	"github.com/ameske/nfl-pickem/sqlite3"
 	"github.com/spf13/cobra"
@@ -18,10 +19,18 @@ var scheduleFile string
 
 func init() {
 	ScheduleCmd.AddCommand(scheduleDownloadCmd)
+	ScheduleCmd.AddCommand(scheduleResultsCmd)
 	ScheduleCmd.AddCommand(scheduleImportCmd)
 
 	scheduleDownloadCmd.Flags().UintVarP(&scheduleYear, "year", "y", 0, "NFL season year")
 	scheduleDownloadCmd.Flags().UintVarP(&scheduleWeek, "week", "w", 0, "NFL season week")
+
+	scheduleResultsCmd.Flags().UintVarP(&scheduleYear, "year", "y", 0, "NFL season year")
+	scheduleResultsCmd.Flags().UintVarP(&scheduleWeek, "week", "w", 0, "NFL season week")
+
+	scheduleResultsCmd.AddCommand(scheduleResultsImportCmd)
+	scheduleResultsImportCmd.Flags().UintVarP(&scheduleYear, "year", "y", 0, "NFL season year")
+	scheduleResultsImportCmd.Flags().UintVarP(&scheduleWeek, "week", "w", 0, "NFL season week")
 
 	scheduleImportCmd.Flags().UintVarP(&scheduleYear, "year", "y", 0, "NFL season year")
 	scheduleImportCmd.Flags().UintVarP(&scheduleWeek, "week", "w", 0, "NFL season week")
@@ -33,6 +42,72 @@ var ScheduleCmd = &cobra.Command{
 	Use:   "schedule",
 	Short: "query or modify the schedule information",
 	Long:  "query or modify the schedule information",
+}
+
+var scheduleResultsImportCmd = &cobra.Command{
+	Use:   "import",
+	Short: "import results into the provided database",
+	Long:  "import results into the provided database",
+	Run: func(cmd *cobra.Command, args []string) {
+		if scheduleYear == 0 || scheduleWeek == 0 {
+			log.Fatal("year and week must be set via command line")
+		}
+
+		// Get a handle to the datastore
+		if datastore == "" {
+			log.Fatal("db flag is required")
+		}
+
+		db, err := sqlite3.NewDatastore(datastore)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results, err := getResultsFromNFL(scheduleYear, scheduleWeek)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if verbose {
+			for _, r := range results {
+				fmt.Fprintln(os.Stderr, r)
+			}
+		}
+
+		for _, r := range results {
+			err := db.UpdateGame(int(scheduleWeek), int(scheduleYear), r.Home, r.HomeScore, r.AwayScore)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	},
+}
+
+var scheduleResultsCmd = &cobra.Command{
+	Use:   "results",
+	Short: "downlaod schedule results from the NFL's website",
+	Long:  "download schedule results from the NFL's website",
+	Run: func(cmd *cobra.Command, args []string) {
+		if scheduleYear == 0 || scheduleWeek == 0 {
+			log.Fatal("year and week must be set via command line")
+		}
+
+		results, err := getResultsFromNFL(scheduleYear, scheduleWeek)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if verbose {
+			for _, r := range results {
+				fmt.Fprintln(os.Stderr, r)
+			}
+		}
+
+		err = json.NewEncoder(os.Stdout).Encode(&results)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
 }
 
 var scheduleDownloadCmd = &cobra.Command{
@@ -115,13 +190,21 @@ func getScheduleFromNFL(year, week uint) ([]schedule.Matchup, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer r.Close()
 
-	return parseSchedule(int(year), r)
+	p := schedule.NewParser(int(year), r)
+
+	return p.Parse()
 }
 
-// parseSchedule parses HTML input into an array of schedule.Matchup
-func parseSchedule(year int, r io.ReadCloser) ([]schedule.Matchup, error) {
-	p := schedule.NewParser(int(year), r)
+func getResultsFromNFL(year, week uint) ([]results.Result, error) {
+	r, err := getScheduleHTML(year, week)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	p := results.NewParser(r)
 
 	return p.Parse()
 }
